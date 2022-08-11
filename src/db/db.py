7f6d7cc82b35a905bd537bfa75be99ba4a2d560c
EPajares <elias.pajares@plan4better.de>
2022-08-11 21:04:54 +0000

    removed not needed db functions

--- src/db/db.py
+++ src/db/db.py
@@ -2,27 +2,15 @@
 # Code based on
 # https://github.com/hackersandslackers/psycopg2-tutorial/blob/master/psycopg2_tutorial/db.py
 import logging as LOGGER
-from multiprocessing import connection
 import psycopg2
-import sys,os
-parentdir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-sys.path.insert(0,parentdir)
-
 from sqlalchemy import create_engine
-from db.config import DATABASE, DATABASE_RD
 
-user,password,host,port,dbname = DATABASE.values()
-user_rd,password_rd,host_rd,port_rd,dbname_rd = DATABASE_RD.values()
 
 class Database:
     """PostgreSQL Database class."""
-    def __init__(self, db_type='processing'):
-        
-        if db_type == 'processing': 
-            self.connection_string = " ".join(("{}={}".format(*i) for i in DATABASE.items()))
-        elif db_type == 'reading':
-            self.connection_string = " ".join(("{}={}".format(*i) for i in DATABASE_RD.items()))
-
+    def __init__(self, db_config: dict):
+        self.db_config = db_config
+        self.connection_string = " ".join(("{}={}".format(*i) for i in db_config.items()))
         try:
             self.conn = psycopg2.connect(self.connection_string)
         except psycopg2.DatabaseError as e:
@@ -31,72 +19,12 @@ class Database:
         finally:
             LOGGER.getLogger().setLevel(LOGGER.INFO)   # To show logging.info in the console
             LOGGER.info('Connection opened successfully.')
-
-    def connect_geopandas(self):
-        if self.conn is None:
-            try:
-                # connection object
-                self.conn = psycopg2.connect(database=dbname, user=user, password=password, host=host, port=port)     
-            except Exception as e:
-                LOGGER.error(e)
-                raise e
-            finally:
-                LOGGER.getLogger().setLevel(LOGGER.INFO)   # To show logging.info in the console
-                LOGGER.info('Connection opened successfully.')
-        return self.conn
-
-    def connect(self):
-        """Connect to a Postgres database."""
-        # if self.conn is None:
-        try:
-            connection_string = " ".join(("{}={}".format(*i) for i in DATABASE.items()))
-            self.conn = psycopg2.connect(connection_string)
-        except psycopg2.DatabaseError as e:
-            LOGGER.error(e)
-            raise e
-        finally:
-            LOGGER.getLogger().setLevel(LOGGER.INFO)   # To show logging.info in the console
-            LOGGER.info('Connection opened successfully.')
-        return self.conn
-      
-    def connect_rd(self):
-        """Connect to a Postgres database."""
-        if self.conn is None:
-            try:
-                connection_string = " ".join(("{}={}".format(*i) for i in DATABASE_RD.items()))
-                self.conn = psycopg2.connect(connection_string)
-            except psycopg2.DatabaseError as e:
-                LOGGER.error(e)
-                raise e
-            finally:
-                LOGGER.info('Connection opened successfully.')
-        return self.conn
-      
-    def connect_sqlalchemy(self):
-        print ("Connect to a Postgres database with engine")
-        
-        # if self.conn is None :    # check if it is connected with engine
-        try:
-            self.conn = create_engine(f"postgresql://{user}:{password}@{host}:{port}/{dbname}", pool_pre_ping=True)
-        except Exception as e:
-            LOGGER.error(e)
-            raise e
-        finally:
-            LOGGER.info('Connection opened successfully.')
-        return self.conn
-        
-    def connect_rd_sqlalchemy(self):
-        """Connect to a Postgres database with engine"""
-        if self.conn is None:
-            try:
-                self.conn = create_engine(f"postgresql://{user_rd}:{password_rd}@{host_rd}:{port_rd}/{dbname_rd}")
-            except Exception as e:
-                LOGGER.error(e)
-                raise e
-            finally:
-                LOGGER.info('Connection opened successfully.')
-        return self.conn
-        
+            
+    def return_sqlalchemy_engine(self):  
+        """This will create SQLAlchemy engine for the database"""
+        conf = self.db_config
+        return create_engine(f'postgresql://{conf["user"]}:{conf["password"]}@{conf["host"]}:{conf["port"]}/{conf["dbname"]}', future=False)
+                
     def select(self, query, params=None):
         """Run a SQL query to select rows from table."""
         with self.conn.cursor() as cur:
@@ -108,18 +36,6 @@ class Database:
         cur.close()
         return records
     
-    def select_rd(self, query, params=None):
-        """Run a SQL query to select rows from table."""
-        self.connect_rd()
-        with self.conn.cursor() as cur:
-            if params is None:
-                cur.execute(query)
-            else:
-                cur.execute(query, params)
-            records = cur.fetchall()
-        cur.close()
-        return records    
-
     def perform(self, query, params=None):
         """Run a SQL query that does not return anything"""
         with self.conn.cursor() as cur:
@@ -130,20 +46,8 @@ class Database:
         self.conn.commit()
         cur.close()
 
-    def perform_rd(self, query, params=None):
-        """Run a SQL query that does not return anything"""
-        self.connect_rd()
-        with self.conn.cursor() as cur:
-            if params is None:
-                cur.execute(query)
-            else:
-                cur.execute(query, params)
-        self.conn.commit()
-        cur.close()
-
     def mogrify_query(self, query, params=None):
         """This will return the query as string for testing"""
-        self.connect()
         with self.conn.cursor() as cur:
             if params is None:
                 result = cur.mogrify(query)
@@ -152,24 +56,6 @@ class Database:
         cur.close()
         return result
 
-    def fetch_one(self, query, params=None):
-        """This will return the next row in the result set"""
-        self.connect()
-        with self.conn.cursor() as cur:
-            cur.execute(query)
-            if not cur:
-                self.send_error(404, f"sql query failed: {(query)}")
-                return None
-            return cur.fetchone()[0]
-
     def cursor(self):
         """This will return the query as string for testing"""
-        self.connect()
-        self.conn.cursor()
-        return self.conn.cursor()
-
-    def cursor_rd(self):
-        """This will return the query as string for testing"""
-        self.connect_rd()
-        self.conn.cursor()
         return self.conn.cursor()
