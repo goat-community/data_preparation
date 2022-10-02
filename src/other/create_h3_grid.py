33e01d81d62e589de882fbffeadbcbccb66c67a2
EPajares <elias.pajares@plan4better.de>
2022-10-02 07:18:32 +0000

    [WIP] GTFS data preparation

--- src/other/create_h3_grid.py
+++ src/other/create_h3_grid.py
@@ -7,7 +7,9 @@ import numpy as np
 from shapely.geometry import Polygon
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir)))
 from db.db import Database
+from src.db.config import DATABASE_RD
 from other.utility_functions import database_table2df
+import json 
 
 class H3Grid:   
     """"This class is used to create a hexgon grid using Uber H3 grid index."""
@@ -18,7 +20,16 @@ class H3Grid:
         Creates a geojson-like dictionary for H3 from a bounding box
         """
         return {'type': 'Polygon', 'coordinates': [[[bottom, left], [bottom, right], [top, right], [top, left], [bottom, left]]]}
-
+   
+    def read_geojson_from_file(self, file_path):
+        geojson = json.load(open(file_path))
+        flipped_coordinates = []
+        for i in geojson["features"][0]["geometry"]['coordinates'][0]:
+            flipped_coordinates.append([i[1], i[0]])
+            
+        geojson["features"][0]["geometry"]['coordinates'] = [flipped_coordinates]
+        return geojson["features"][0]["geometry"]
+    
     def create_grid(self, polygon, resolution, layer_name):
         """
         Creates a h3 grid for passed bounding box in SRID 4326
@@ -44,17 +55,28 @@ class H3Grid:
         hex_ids = pd.Series(hex_ids)
         gdf = gpd.GeoDataFrame(pd.concat([hex_ids,hex_parents,hex_polygons], keys=['id','parent_id','geometry'], axis=1))
         
-        # Save as Geopackage
+        return gdf.astype({"id": int, "parent_id": int})
+    
+    def export_to_geopackage(self, gdf, layer_name):
+        """
+        Exports a GeoDataFrame to a geopackage
+        """
         gdf.to_file('%s/%s.gpkg' % (self.data_dir,layer_name), driver='GPKG')
-       
+    
+    def save_to_db(self, gdf, layer_name, engine):
+        gdf.to_postgis(layer_name, engine, if_exists='replace', index=False)
 
+# db = Database(DATABASE_RD)
+# db_engine = db.return_sqlalchemy_engine()
 # Create grid from bouding box example
 #bbox = H3Grid().create_geojson_from_bbox(top=48.352598707539315, left=11.255493164062498, bottom=47.92738566360356, right=11.8927001953125)
-bbox = H3Grid().create_geojson_from_bbox(top=26.33345, left=-80.17602, bottom=26.22792, right=-80.06851)
-H3Grid().create_grid(polygon=bbox, resolution=9, layer_name='grid_visualization')
-H3Grid().create_grid(polygon=bbox, resolution=10, layer_name='grid_calculation')
-
-
+#bbox = H3Grid().create_geojson_from_bbox(top=50.621, left=8.532, bottom=47.546, right=13.869)
+# h3_grid = H3Grid()
+# bbox = h3_grid.read_geojson_from_file('/app/src/data/input/clip.geojson')
+# gdf = h3_grid.create_grid(polygon=bbox, resolution=9, layer_name='grid_visualization')
+# h3_grid.save_to_db(gdf, layer_name='grid_visualization', engine=db_engine)
+# gdf = h3_grid.create_grid(polygon=bbox, resolution=10, layer_name='grid_calculation')
+# h3_grid.save_to_db(gdf, layer_name='grid_calculation', engine=db_engine)
 # bbox = H3Grid().create_geojson_from_bbox(top=48.0710465709655708, left=7.6619009582867594, bottom=47.9035964575060191, right=7.9308570081842120)
 # H3Grid().create_grid(polygon=bbox, resolution=9, layer_name='grid_visualization')
 # H3Grid().create_grid(polygon=bbox, resolution=10, layer_name='grid_calculation')
---
+++ src/other/gtfs_preparation.sql
@@ -0,0 +1,156 @@
+DROP TABLE IF EXISTS temporal.test_train_routes; 
+CREATE TABLE temporal.test_train_routes AS 
+WITH train_routes AS 
+(
+	SELECT r.*, a.agency_name 
+	FROM gtfs_new.routes r, gtfs_new.agency a  
+	WHERE route_type = '2'
+	AND r.agency_id = a.agency_id 
+),
+selected_trips_routes AS 
+(
+	SELECT r.*, j.shape_id
+	FROM train_routes r
+	CROSS JOIN LATERAL 
+	(
+		SELECT t.* 
+		FROM gtfs_new.trips t
+		WHERE t.route_id = r.route_id  
+		LIMIT 1 
+	) j
+)
+SELECT t.*, s.*
+FROM selected_trips_routes t
+CROSS JOIN LATERAL 
+(
+	SELECT ST_MakeLine(shape_pt_loc) AS geom 
+	FROM gtfs_new.shapes s
+	WHERE s.shape_id = t.shape_id 
+) s;
+CREATE INDEX ON temporal.test_train_routes USING GIST(geom);
+
+
+DROP TABLE IF EXISTS temporal.test_train_routes_bavaria;
+CREATE TABLE temporal.test_train_routes_bavaria AS 
+SELECT DISTINCT w.*
+FROM temporal.test_train_routes w, basic.study_area s
+WHERE ST_Intersects(s.geom, w.geom)
+AND s.id::text LIKE '9%';
+CREATE INDEX ON temporal.test_train_routes_bavaria USING GIST(geom);
+
+agency_names_out =  ['Döllnitzbahngesellschaft', 'Wanderbahn im Regental', 'Nichtbundeseigene Eisenbahnen', 'Mainschleifenbahn', 'Chiemgauer Lokalbahn', 'Rhön-Zügle']
+agency_name_long_distance = ['Österreichische Bundesbahnen', 'Nachtzug']
+route_lomg_names_out = ['Sonderzug', ]
+
+
+agency_to_bus= Schmetterling Reisen, DB ZugBus Regionalverkehr Alb-Bodensee, Regensburger Verkehrsverbund
+Gesellschaft zur Förderung des ÖPNV Regensburg
+Landkreis Landsberg (Lech)
+
+
+--Label touristic trains
+WITH agency_ids AS 
+(
+	SELECT agency_id 
+	FROM gtfs_new.agency a 
+	WHERE a.agency_name IN ('Döllnitzbahngesellschaft', 'Wanderbahn im Regental', 'Nichtbundeseigene Eisenbahnen', 'Mainschleifenbahn', 'Chiemgauer Lokalbahn', 'Rhön-Zügle') 
+)
+UPDATE gtfs_new.routes r
+SET route_type = '107'
+WHERE r.agency_id IN (SELECT * FROM agency_ids) 
+
+--Label wrongly labelled train as bus 
+WITH agency_ids AS 
+(
+	SELECT a.*
+	FROM gtfs_new.agency a 
+	WHERE a.agency_name IN ('Schmetterling Reisen', 'DB ZugBus Regionalverkehr Alb-Bodensee', 
+	'Regensburger Verkehrsverbund', 'Gesellschaft zur Förderung des ÖPNV Regensburg', 'Landkreis Landsberg (Lech)') 
+)
+UPDATE gtfs_new.routes r
+SET route_type = '3'
+WHERE r.agency_id IN (SELECT agency_id  FROM agency_ids); 
+
+UPDATE gtfs_new.routes r 
+SET route_type = '107'
+WHERE route_long_name = 'Sonderzug';
+
+UPDATE gtfs_new.routes r
+SET route_type = '101'
+WHERE route_type = '2'
+AND route_long_name LIKE ANY (array['ICE', 'EC', 'RJX', 'EuroCity-Express', 'Intercity-Express']); 
+
+UPDATE gtfs_new.routes r
+SET route_type = '102'
+WHERE route_type = '2'
+AND route_long_name LIKE ANY (array['EuroCity', 'Intercity', 'IC']);  
+
+UPDATE gtfs_new.routes r
+SET route_type = '105'
+WHERE route_type = '2'
+AND route_long_name LIKE ANY (array['EuroNight', 'NJ', 'EN']);  
+
+--Label Flixtrain as Long  Distance
+UPDATE gtfs_new.routes r 
+SET route_type = '102'
+WHERE route_type = '2'
+AND agency_id = '80';
+
+
+--Reclassify Bus services
+--Label on demand services
+UPDATE gtfs_new,routes r 
+SET route_type = '715'
+WHERE route_short_name LIKE 'AST%'; 
+
+--Label on demand services in MVV
+UPDATE gtfs_new.routes r 
+SET route_type = '715'
+WHERE length(regexp_replace(route_short_name, '[^0-9]', '', 'g')) = 4
+AND agency_id = '14'
+AND route_type = '3'; 
+
+--Label Flixbus as Coach
+UPDATE gtfs_new.routes r 
+SET route_type = '200'
+WHERE agency_id = '258'
+AND route_type = '3'; 
+
+
+DROP TABLE IF EXISTS gtfs_new.trips_weekday;
+CREATE TABLE gtfs_new.trips_weekday (id serial, trip_id TEXT, route_type gtfs_new."route_type_val", weekdays boolean[]);
+INSERT INTO gtfs_new.trips_weekday(trip_id, route_type, weekdays)
+SELECT t.trip_id, t.route_type, ARRAY[
+(('{"available": "true", "not_available": "false"}'::jsonb) ->> c.monday::text)::boolean,
+(('{"available": "true", "not_available": "false"}'::jsonb) ->> c.tuesday::text)::boolean,
+(('{"available": "true", "not_available": "false"}'::jsonb) ->> c.wednesday::text)::boolean,
+(('{"available": "true", "not_available": "false"}'::jsonb) ->> c.thursday::text)::boolean,
+(('{"available": "true", "not_available": "false"}'::jsonb) ->> c.friday::text)::boolean,
+(('{"available": "true", "not_available": "false"}'::jsonb) ->> c.saturday::text)::boolean,
+(('{"available": "true", "not_available": "false"}'::jsonb) ->> c.sunday::text)::boolean
+] AS weekdays
+FROM
+ (
+ SELECT t.trip_id, t.service_id, r.route_type
+	FROM gtfs_new.trips t, gtfs_new.routes r
+    WHERE t.route_id = r.route_id
+ ) t, gtfs_new.calendar c
+WHERE t.service_id = c.service_id
+AND '2022-05-16' >= start_date
+AND '2022-05-22' <= end_date;
+ALTER TABLE gtfs_new.trips_weekday ADD PRIMARY KEY (id);
+CREATE INDEX ON gtfs_new.trips_weekday (trip_id);
+CREATE TABLE gtfs_new.stop_times_optimized (
+    id serial4 NOT NULL,
+    trip_id text NULL,
+    arrival_time interval NULL,
+    stop_id text NULL,
+    route_type gtfs_new."route_type_val" NULL,
+    weekdays _bool NULL
+);
+INSERT INTO gtfs_new.stop_times_optimized(trip_id, arrival_time, stop_id, route_type, weekdays)
+SELECT st.trip_id, st.arrival_time, stop_id, route_type, weekdays
+FROM gtfs_new.stop_times st, gtfs_new.trips_weekday w
+WHERE st.trip_id = w.trip_id;
+ALTER TABLE gtfs_new.stop_times_optimized ADD PRIMARY KEY(id);
+CREATE INDEX ON gtfs_new.stop_times_optimized(stop_id, arrival_time);
