aa8b3f7ca8272b3361d756467b75d80420a29afe
irem kapucuoglu <i.kapucuoglu@gmail.com>
2022-12-06 13:23:39 +0000

    [WIP] Updates on process LOD2 buildings
    Co-authored-by: Elias Pajares <EPajares@users.noreply.github.com>

--- src/collection/citygml_collection.py
+++ src/collection/citygml_collection.py
@@ -8,7 +8,7 @@ import pprint
 
 
 class CityGMLCollection:
-    def __init__(self, db_config):
+    def __init__(self, db_config: dict):
          self.dbname = db_config["dbname"]
          self.host = db_config["host"]
          self.username = db_config["user"]
@@ -18,7 +18,7 @@ class CityGMLCollection:
          self.data_dir_input = self.root_dir + "/src/data/input/"
          self.temp_data_dir = self.data_dir_input + "temp/"
          create_pgpass()
-         tree = ET.parse("buildings.xml")
+         tree = ET.parse("building_function_types_citygml.xml")
          root = tree.getroot()
          dictentry = root.findall('{http://www.opengis.net/gml}dictionaryEntry')
          dict_building_types = {}
@@ -31,40 +31,61 @@ class CityGMLCollection:
                 
          self.dict_building_types = dict_building_types
 
-    def read_buildings(self, db, geom_wkt):
-         buildings = db.select(f""" WITH merged_buildings AS 
+    def read_buildings(self, db, geom_wkt: str):
+         buildings = db.select(f"""WITH merged_buildings AS 
         (
             SELECT s.root_id, s.cityobject_id, b.roof_type, b.measured_height, b.function, s.geometry AS geom 
             FROM surface_geometry s, building b
-            WHERE ST_Intersects(s.geometry,  ST_TRANSFORM(ST_Geomfromtext('{geom_wkt}', 4326),25832))
+            WHERE ST_Intersects(s.geometry, ST_TRANSFORM(ST_Geomfromtext('{geom_wkt}', 4326), 25832))
             AND geometry IS NOT NULL 
             AND ST_GeometryType (ST_FORCE2D(ST_MakeValid(s.geometry))) = 'ST_Polygon'
             AND cityobject_id = b.id 
+        ), 
+        unioned_buildings AS 
+        (
+            SELECT m.root_id, roof_type, measured_height, 
+            function, ST_UNION(ST_FORCE2D(ST_makeValid(m.geom))) AS geom 
+            FROM merged_buildings m, thematic_surface t
+            WHERE m.cityobject_id = t.building_id 
+            GROUP BY m.root_id, roof_type, measured_height, function
         )
-        SELECT m.root_id, roof_type, measured_height, 
-        function, ST_AsText(ST_UNION(ST_FORCE2D(ST_makeValid(m.geom)))) AS geom 
-        FROM merged_buildings m, thematic_surface t
-        WHERE m.cityobject_id = t.building_id 
-        GROUP BY m.root_id, roof_type, measured_height, function;""")
+        SELECT root_id, roof_type, measured_height, function, ST_AsText((ST_DUMP(geom)).geom)
+        FROM unioned_buildings;""")
         
          return buildings
          
-            
-    
-    def processing_units(self, db, db_rd):
-         Config("buildings").download_db_schema()
-         create_table_schema(db, DATABASE, "basic.building")
-         processing_units = db.select("""SELECT ST_AsText(ST_MULTI(create_equal_area_split_polygon(hb.geom, 25)))
-                                      FROM hamburg_boundries hb """)
-         
-         
-                  
-         # Read buildings from the database
-         for processing_unit in processing_units:
-             print('Processing unit will be computed:' + str(processing_units.index(processing_unit)))
-             buildings= self.read_buildings(db_rd, processing_unit[0])
-             self.insert_buildings(db, buildings)
-            
+    def processing_units(self, db, db_rd, boundary_name: str, on_exists: str = "append"):   
+
+        if on_exists == "replace":
+            print("Building table will be created and data will be inserted.")       
+            Config("buildings").download_db_schema()   
+            create_table_schema(db, DATABASE, "basic.building")
+        elif on_exists == 'append':
+            check_if_exists = db.select("""
+                SELECT EXISTS (
+                    SELECT FROM information_schema.tables 
+                    WHERE  table_schema = 'basic'
+                    AND    table_name   = 'building'
+                );"""
+            )
+            if check_if_exists[0][0] == True: 
+                print("Data will be appended to the existing building table.")    
+            else: 
+                raise NameError("The building table does not exist. Therefore the data cannot be appended")
+        else:
+            raise ValueError("Please provide a valid argument for the parameter 'on_exists'.")            
+        
+        processing_units = db.select(
+            f"""SELECT ST_AsText(ST_MULTI(create_equal_area_split_polygon(bb.geom, 25)))
+            FROM basic.building_boundaries bb 
+            WHERE name = '{boundary_name}'"""
+        )
+        # Read buildings from the database
+        for processing_unit in processing_units:
+            print('Processing unit will be computed:' + str(processing_units.index(processing_unit)))
+            buildings= self.read_buildings(db_rd, processing_unit[0])
+            self.insert_buildings(db, buildings)
+        
      # Insert buildings into the database
     def insert_buildings (self, db, buildings):
         for building in buildings:
@@ -82,8 +103,12 @@ db = Database(DATABASE)
 db_rd= Database(DATABASE_RD)
 
 city_gml_collection = CityGMLCollection(DATABASE)
-city_gml_collection.processing_units(db,db_rd)
-city_gml_collection.insert_buildings(db)
+city_gml_collection.processing_units(db, db_rd, boundary_name="Bremen", on_exists="replace") # Please note the first time you need create the table
+
+
+
+
+
 
 
 
