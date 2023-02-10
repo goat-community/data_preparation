0ceda9c9fcb24d73ffdbb62a4661f250dd4d9298
Ebubeker Rexha <60944813+Ebubeker@users.noreply.github.com>
2023-02-10 11:03:00 +0000

    The script for modifying the traffic data is ready

--- src/preparation/network_car.py
+++ src/preparation/network_car.py
@@ -1,21 +1,25 @@
-from src.db.db import Database
+import glob
+import xml.etree.ElementTree as ET
+
 from src.core.config import settings
+from src.db.db import Database
+
 
 class NetworkCar:
     def __init__(self, db, time_of_the_day):
         self.db = db
         self.bulk_size = 100000
-        
+
         sql_query_cnt = "SELECT COUNT(*) FROM dds_street_with_speed"
         cnt_network = self.db.select(sql_query_cnt)
         self.cnt_network = cnt_network[0][0]
-        
-        # Convert the time to the column name 
-        self.time_of_the_day = "h"+time_of_the_day.replace(":", "_")
 
-    #TODO: We can try to make this a bit flexible and allow the user to pass different times of the day
+        # Convert the time to the column name
+        self.time_of_the_day = "h" + time_of_the_day.replace(":", "_")
+
+    # TODO: We can try to make this a bit flexible and allow the user to pass different times of the day
     # Weekdays we are not having currently though in the database
-    
+
     def create_network_nodes(self):
         # Create table for the nodes of the network
         sql_create_node_table = """
@@ -28,8 +32,8 @@ class NetworkCar:
             CREATE INDEX ON dds_street_nodes (coords);
         """
         self.db.perform(sql_create_node_table)
-        
-        # Create nodes of the network in batches 
+
+        # Create nodes of the network in batches
         for offset in range(0, self.cnt_network, self.bulk_size):
             sql_get_nodes = f"""
                 INSERT INTO dds_street_nodes (coords)
@@ -53,12 +57,10 @@ class NetworkCar:
                 WHERE o.id IS NULL;
             """
             self.db.perform(sql_get_nodes)
-    
-    
+
     def read_network_car(self):
-        """Export the car network from the database for certain times of the day.
-        """       
-        
+        """Export the car network from the database for certain times of the day."""
+
         for offset in range(0, self.cnt_network, self.bulk_size):
             sql_query_read_network_car = f"""
                 WITH batch_ways AS 
@@ -105,14 +107,96 @@ class NetworkCar:
             """
             result = self.db.select(sql_query_read_network_car)
             result = [x[0] for x in result]
-            print()
-            #TODO: Convert to OSM format
-    
+
+            header = """<?xml version="1.0" encoding="UTF-8"?><osm version="0.6" generator="Overpass API 0.7.59 e21c39fe">"""
+
+            footer = """</osm>"""
+
+            properties = ""
+
+            # result_data = (properties + way for way in result);
+            for indx, resultItem in enumerate(result):
+                print(f"count: {indx}")
+                properties = properties + resultItem
+
+            xmlFileContent = header + properties + footer
+
+            with open(f"{offset}offset.osm", "w") as f:
+                f.write(xmlFileContent)
+
+            # TODO: Convert to OSM format
+
+    def collide_all_data(self):
+        """
+        Here we get all the nodes and create them into xml tags in order to add them to a common file
+        """
+        print("Creating the nodes...")
+        nodes = ""
+        sql_get_nodes = """
+            SELECT *
+            FROM public.dds_street_nodes;
+        """
+        results = self.db.select(sql_get_nodes)
+        for result in results:
+            nodes = (
+                nodes
+                + f'<node id="{result[0]}" lat="{result[1][0]}" lon="{result[1][1]}"/>'
+            )
+        print("Created all the nodes")
+
+        """
+           Here we collide all the nodes into one file since their geometries will refer to the nodes that we go above.
+           They need to be in the same file
+        """
+
+        ways = ""
+        print("Merging all the ways...")
+        for offset in range(0, self.cnt_network, self.bulk_size):
+            node = None
+            fileinfo = ET.parse(f"{offset}offset.osm")
+            root = fileinfo.getroot()
+            if node is None:
+                node = root
+            elements = root.find("./way")
+            for element in elements.iter():
+                node[1].append(element)
+
+            ways = ways + str(ET.tostring(node), "UTF-8")
+        print("Finished merging the ways")
+        self.putAllTogether(nodes=nodes, ways=ways)
+
+        """
+            Here we put together all the things
+            header - Info about the file and file format
+            footer - closing tag for header
+            ways - includes all the roads
+            nodes - includes all the references to geopoints in the ways
+        """
+
+    def putAllTogether(self, nodes, ways):
+        print("Outputing the data")
+        header = """<?xml version="1.0" encoding="UTF-8"?><osm version="0.6" generator="Overpass API 0.7.59 e21c39fe">"""
+
+        footer = """</osm>"""
+
+        allData = header + ways + nodes + footer
+
+        with open("traffic_data.osm", "w") as f:
+            f.write(allData)
+            print("Data Outputed")
+            print("Finished")
+
+
 def main():
     """Main function."""
     db = Database(settings.REMOTE_DATABASE_URI)
-    network_car = NetworkCar(db=db, time_of_the_day="08:00")   
+    network_car = NetworkCar(db=db, time_of_the_day="08:00")
+    print("Creating the files...")
     network_car.read_network_car()
-    
+    print("Files have been created")
+    print("Started colliding all the files into one...")
+    network_car.collide_all_data()
+
+
 if __name__ == "__main__":
     main()
