2ceb99e0177e27c20c60cacc2cbe9e905d51d025
Philipp Otter <philipp-otter@t-online.de>
2023-09-10 17:40:21 +0200

    [WIP] added multipoint geom

--- src/preparation/public_transport_stop.py
+++ src/preparation/public_transport_stop.py
@@ -11,8 +11,6 @@ class PublicTransportStopPreparation:
     def __init__(self, db: Database, region: str):
         self.db = db
         self.region = region
-
-        # Get variables for the classification
         self.config_public_transport_stop = Config("public_transport_stop", region).config['classification'] 
             
     def run(self):
@@ -24,10 +22,9 @@ class PublicTransportStopPreparation:
         self.db.perform(create_poi_table(data_set_type="poi", schema_name="basic", data_set="public_transport_stop"))  
               
         category_columns = ", ".join([f"category_{i}" for i in range(1, len(self.config_public_transport_stop['gtfs_route_types']) + 1)])
-        category_aliases = ', '.join([f"route_types[{i}] AS category{i}" for i in range(1, len(self.config_public_transport_stop['gtfs_route_types']) + 1)])
+        category_aliases = ', '.join([f"route_types[{i}] AS category_{i}" for i in range(1, len(self.config_public_transport_stop['gtfs_route_types']) + 1)])
 
         for id in unique_study_area_ids:
-            print(id)
             classify_gtfs_stop_sql = f"""
                 INSERT INTO basic.poi_public_transport_stop(
                     {category_columns},
@@ -43,15 +40,15 @@ class PublicTransportStopPreparation:
                     AND parent_station IS NULL
                 ),
                 clipped_gfts_stops AS (
-                    SELECT ST_MULTI(ST_UNION(s.stop_loc)) AS geom, p.name, json_build_object('stop_id', ARRAY_AGG(s.stop_id)) AS tags
+                    SELECT p.name, s.stop_loc AS geom, json_build_object('stop_id', s.stop_id, 'parent_station', s.parent_station) AS tags
                     FROM gtfs.stops s, basic.study_area a, parent_station_name p
                     WHERE ST_Intersects(s.stop_loc, a.geom)
                     AND a.id = {id[0]}
                     AND parent_station IS NOT NULL
                     AND s.parent_station = p.stop_id
-                    GROUP BY s.parent_station, p.name
-                )
-                SELECT {category_aliases}, c.*
+                ), 
+                categorized_gtfs_stops AS (
+                SELECT {category_aliases}, c.name, c.geom, c.tags
                 FROM clipped_gfts_stops c 
                 CROSS JOIN LATERAL 
                 (
@@ -69,12 +66,15 @@ class PublicTransportStopPreparation:
                         ORDER BY r.route_type
                     ) x 
                 ) j
+                )
+                SELECT {category_columns}, name, ST_MULTI(ST_UNION(geom)) AS geom, json_build_object('stop_id', ARRAY_AGG(tags ->> 'stop_id')) AS tags
+                FROM categorized_gtfs_stops
+                WHERE category_1 IS NOT NULL
+                GROUP BY {category_columns}, tags ->> 'parent_station', name
                 ;
             """
 
             self.db.perform(classify_gtfs_stop_sql)
-            
-    #TODO: drop entries without category (category_1 = NULL etc.)
         
 def prepare_public_transport_stop(region: str):
 
