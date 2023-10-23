ae8f04cd08338cef50cbde7481573fdde285016d
EPajares <elias.pajares@plan4better.de>
2023-10-23 11:11:49 +0000

    Fixed initdb.py to create db

--- initdb.py
+++ initdb.py
@@ -1,8 +1,14 @@
 import os
-
-from src.core.config import settings
 from src.db.db import Database
 from src.utils.utils import print_error, print_info
+from src.core.config import settings
+import subprocess
+
+def create_db():
+    """Create DataPreparation database."""
+    # Connect to default database
+    db_name = settings.POSTGRES_DB
+    subprocess.run(["psql", "-U", "rds", "-d", "postgres", "-c", f"CREATE DATABASE {db_name};"])
 
 
 def init_db(db):
@@ -25,12 +31,13 @@ def init_db(db):
                 db.perform(f.read())
 
 if __name__ == "__main__":
-    db = Database(settings.LOCAL_DATABASE_URI)
-    try:
-        init_db(db)
-        print_info("Database initialized.")
-    except Exception as e:
-        print_error(e)
-        print_error("Database initialization failed.")
-    finally:
-        db.close()
+    create_db()
+    #db = Database(settings.LOCAL_DATABASE_URI)
+    # try:
+    #     init_db(db)
+    #     print_info("Database initialized.")
+    # except Exception as e:
+    #     print_error(e)
+    #     print_error("Database initialization failed.")
+    # finally:
+    #     db.close()
