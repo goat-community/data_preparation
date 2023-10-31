a06649074fcdbb6cea7f23c4c6d7bee43988fc66
Philipp-Otter <philipp-otter@t-online.de>
2023-10-31 10:46:20 +0000

    new POI table schema

--- src/db/tables/poi.py
+++ src/db/tables/poi.py
@@ -3,12 +3,10 @@ class POITable:
         self.data_set_type = data_set_type
         self.data_set_name = data_set_name
         self.schema_name = schema_name
-        self.table_name = f"{self.data_set_type}_{self.data_set_name}"
-        self.index_columns = "geom"
-        self.index_method = "gist"
 
-        # Define common columns
-        self.common_columns = [
+    def create_table(self, table_name: str, category_columns: list) -> str:
+        # Common columns for all POI tables
+        common_columns = [
             "name text NULL",
             "operator text NULL",
             "street text NULL",
@@ -21,46 +19,48 @@ class POITable:
             "opening_hours text NULL",
             "wheelchair text NULL",
             "source text NULL",
-            "tags jsonb NULL",
+            "tags jsonb DEFAULT '{"'"extended_source"'": {}}'::jsonb",
             "geom geometry NOT NULL",
-            "auto_pk SERIAL NOT NULL",
-            f"CONSTRAINT {self.table_name}_pkey PRIMARY KEY (auto_pk)"
+            "id SERIAL NOT NULL",
+            f"CONSTRAINT {table_name}_pkey PRIMARY KEY (id)"
         ]
 
-    def create_table(self, table_name: str, category_columns: list) -> str:
-        all_columns = category_columns + self.common_columns
+        # Combine category columns with common columns
+        all_columns = category_columns + common_columns
+
+        # Create SQL query for table creation
         all_columns_str = ",\n".join(all_columns)
         sql_create_table = f"""
             DROP TABLE IF EXISTS {self.schema_name}.{table_name};
             CREATE TABLE {self.schema_name}.{table_name} (
                 {all_columns_str}
             );
-            CREATE INDEX ON {self.schema_name}.{table_name} USING {self.index_method} ({self.index_columns});
+            CREATE INDEX ON {self.schema_name}.{table_name} USING gist (geom);
             """
         return sql_create_table
 
-    def create_poi_table(self) -> str:
-        table_name = f"{self.data_set_type}_{self.data_set_name}"
-        category_columns = [
-            "category text NULL",
-            "other_categories text[] NULL"
-        ]
-        return self.create_table(table_name, category_columns)
+    def create_poi_table(self, table_type: str = 'standard') -> str:
+        if table_type == "standard":
+            table_name = f"{self.data_set_type}_{self.data_set_name}"
+            category_columns = [
+                "category text NULL",
+                "other_categories text[] NULL"
+            ]
+        elif table_type == "school":
+            table_name = f"{self.data_set_type}_school_{self.data_set_name}"
+            category_columns = [
+                "school_isced_level_1 bool NULL",
+                "school_isced_level_2 bool NULL",
+                "school_isced_level_3 bool NULL"
+            ]
+        elif table_type == "childcare":
+            table_name = f"{self.data_set_type}_childcare_{self.data_set_name}"
+            category_columns = [
+                "nursery bool NULL",
+                "kindergarten bool NULL",
+                "after_school bool NULL"
+            ]
+        else:
+            raise ValueError("Invalid table_type. Supported values are 'standard', 'school', or 'childcare'.")
 
-    def create_poi_childcare(self) -> str:
-        table_name = f"{self.data_set_type}_childcare_{self.data_set_name}"
-        category_columns = [
-            "nursery bool NULL",
-            "kindergarten bool NULL",
-            "after_school bool NULL"
-        ]
-        return self.create_table(table_name, category_columns)
-
-    def create_poi_school(self) -> str:
-        table_name = f"{self.data_set_type}_school_{self.data_set_name}"
-        category_columns = [
-            "school_isced_level_1 bool NULL",
-            "school_isced_level_2 bool NULL",
-            "school_isced_level_3 bool NULL"
-        ]
         return self.create_table(table_name, category_columns)
