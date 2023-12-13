3ab7d0c0c4c7742a21982bb88a0ac52b6d217343
Nihar Thakkar <nihartar@gmail.com>
2023-12-13 12:00:57 +0000

    Resolve merge conflicts

--- manage.py
+++ manage.py
@@ -1,15 +1,13 @@
 import os
+
 import typer
+
 from src.collection.building import collect_building
 from src.collection.gtfs import collect_gtfs
 from src.collection.landuse import collect_landuse
 from src.collection.network import collect_network
-from src.collection.network_pt import collect_network_pt
 from src.collection.network_overture import collect_overture_network
-from src.preparation.network import prepare_network
-from src.preparation.poi import prepare_poi
-from src.preparation.network import export_network
-from src.preparation.network_pt import prepare_network_pt
+from src.collection.network_pt import collect_network_pt
 from src.collection.poi import collect_poi
 from src.core.config import settings
 from src.db.db import Database
@@ -18,11 +16,11 @@ from src.preparation.building import prepare_building
 from src.preparation.gtfs import export_gtfs, prepare_gtfs
 from src.preparation.network import export_network, prepare_network
 from src.preparation.network_overture import prepare_overture_network
+from src.preparation.network_pt import prepare_network_pt
 from src.preparation.poi import export_poi, prepare_poi
 from src.preparation.population import prepare_population
 from src.preparation.public_transport_stop import prepare_public_transport_stop
 from src.utils.utils import print_hashtags, print_info
-
 
 app = typer.Typer()
 
