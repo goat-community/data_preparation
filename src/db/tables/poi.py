af7f0c0856c6386e1d2cd6ca715290d66df879a7
CyrkaFRDE <159411247+CyrkaFRDE@users.noreply.github.com>
2024-11-21 09:58:13 +0100

    refactor: gtfs poi table

--- src/db/tables/poi.py
+++ src/db/tables/poi.py
@@ -68,6 +68,13 @@ class POITable:
                 "carrier text NULL",
                 "carrier_type text NULL"
             ]
+        elif table_type == "transport":
+            table_name = f"{self.data_set_type}_{self.data_set_name}"
+            category_columns = [
+                "stop_id text NULL",
+                "category text NULL",
+                "modes text[] NULL"
+            ]
         else:
             raise ValueError("Invalid table_type. Supported values are 'standard', 'school', or 'childcare'.")
 
